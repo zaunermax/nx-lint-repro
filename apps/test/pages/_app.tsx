cce785e8110f0c6b10937f513029c678527465e5
zaunermax <zaunermax@gmail.com>
2021-12-13 11:20:50 +0100

    feat(lint): deliberately added linting errors

--- apps/test/pages/_app.tsx
+++ apps/test/pages/_app.tsx
@@ -1,8 +1,10 @@
-import { AppProps } from 'next/app';
+//FIXME: linter should warn here
+import { AppProps, AppInitialProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const test = 123; //FIXME: linter should warn here
   return (
     <>
       <Head>
